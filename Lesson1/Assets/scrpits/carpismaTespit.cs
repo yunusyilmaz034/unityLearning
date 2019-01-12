0c34f03a5d26224fdd8c85a945dac3502a427943
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-01-12 13:27:35 +0300

    lesson1 finish

---
+++ Lesson1/Assets/scrpits/carpismaTespit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carpismaTespit : MonoBehaviour
+{
+    public int puan;
+    private List<GameObject> puanObjeleri = new List<GameObject>();
+    private void OnCollisionEnter(Collision collision)
+    {
+        puanObjeleri.Add(GameObject.Find("PositiveObject")); // 3+ 2- olacak
+        puanObjeleri.Add(GameObject.Find("PositiveObject"));
+        puanObjeleri.Add(GameObject.Find("PositiveObject"));
+        puanObjeleri.Add(GameObject.Find("NegativeObject"));
+        puanObjeleri.Add(GameObject.Find("NegativeObject"));
+        int secimDegiskeni = Random.Range(0, 5);
+        float rastgeleXDegiskeni = Random.Range(-5.27f, 6.55f);
+        float rastgeleZDegiskeni = Random.Range(-18.67f, - 0.1f);
+        GameObject yaratilanObje = (GameObject)Instantiate(puanObjeleri[secimDegiskeni], new Vector3(rastgeleXDegiskeni, 4f, rastgeleZDegiskeni), Quaternion.Euler(0, 0, 0));
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(1, 3, 4);
+    }
+}
