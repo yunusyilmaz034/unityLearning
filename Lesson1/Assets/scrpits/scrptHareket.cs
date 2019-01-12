a455b33f7725f0feeee238d5d830c955ab7e61ee
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-01-12 11:43:40 +0300

    Hareket Scripti Yazıldı
    Controller hareket scripti temel düzeyde tamamlandı.

--- Lesson1/Assets/scrpits/scrptHareket.cs
+++ Lesson1/Assets/scrpits/scrptHareket.cs
@@ -6,39 +6,38 @@ public class scrptHareket : MonoBehaviour
 {
     public int speed = 0;
     // Start is called before the first frame update
+    Rigidbody rBody;
     void Start()
     {
         Debug.Log(string.Format("x : {0}", gameObject.transform.position.x.ToString()));
         Debug.Log(string.Format("y : {0}", gameObject.transform.position.y.ToString()));
         Debug.Log(string.Format("z : {0}", gameObject.transform.position.z.ToString()));
-
+        Debug.Log(string.Format("speed: {0}", speed));
+        rBody = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    { 
-        if (Input.GetKey(KeyCode.A)) {
-
-            gameObject.transform.position = new Vector3((gameObject.transform.position.x - speed) * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rBody.AddForce(0, 0, speed);
         }
-
-       
-       /*
-        if(Input.GetAxis("Horizontal") < 0.0) //left
+        else if (Input.GetKey(KeyCode.S))
         {
-            gameObject.transform.position = new Vector3((gameObject.transform.position.x - speed) * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
-             
-        } else //rigth
-        {
-            gameObject.transform.position = new Vector3((gameObject.transform.position.x + speed) * Time.deltaTime, gameObject.transform.position.y, gameObject.transform.position.z);
+            rBody.AddForce(0, 0, -speed);
         }
-        if (Input.GetAxis("Vertical") < 0.0) // down
+        else if (Input.GetKey(KeyCode.A))
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, (gameObject.transform.position.z - speed) * Time.deltaTime);
-        } else //up
-        {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, (gameObject.transform.position.z + speed) * Time.deltaTime);
+            rBody.AddForce(-speed, 0, 0);
         }
-        */
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rBody.AddForce(speed, 0, 0);
+        }
+        else
+        {
+            Debug.Log("Herhangi bir tuşa basıldı.");
+        }
     }
 }
