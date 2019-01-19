a747c8b535784ab9a284aae828845e7fef359f96
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-01-19 13:28:17 +0300

    4.hafta
    ders sonu

--- Lesson1/Assets/scrpits/carpismaTespit.cs
+++ Lesson1/Assets/scrpits/carpismaTespit.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class carpismaTespit : MonoBehaviour
 {
     public int puan;
@@ -20,6 +22,11 @@ public class carpismaTespit : MonoBehaviour
         float rastgeleXDegiskeni = Random.Range(-5.27f, 6.55f);
         float rastgeleZDegiskeni = Random.Range(-18.67f, - 0.1f);
         toplamPuan += puan;
+        if (toplamPuan >= 30)
+        {
+            toplamPuan = 0;
+            SceneManager.LoadScene(1);
+        }
         puanYazisi.GetComponent<Text>().text = "Puan: " + toplamPuan.ToString();
         GameObject yaratilanObje = (GameObject)Instantiate(puanObjeleri[secimDegiskeni], new Vector3(rastgeleXDegiskeni, 4.77f, rastgeleZDegiskeni), Quaternion.Euler(0, 0, 0));
         Destroy(gameObject);
