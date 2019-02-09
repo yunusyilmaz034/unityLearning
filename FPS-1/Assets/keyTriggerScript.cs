3e560b79ab22c120f652cdf0e9c4c91933bcb1e0
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-02-09 10:11:11 +0300

    FPS-1
    fps giriş

---
+++ FPS-1/Assets/keyTriggerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keyTriggerScript : MonoBehaviour
+{
+    public Text TriggerText;
+    // Start is called before the first frame updat
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerText.enabled = false;
+        
+        if (Input.GetAxis("TriggerLight") > 0f)
+        {
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        TriggerText.enabled = false;
+    }
+}
