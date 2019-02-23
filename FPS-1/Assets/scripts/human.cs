d34702c9730c43ee7c42d05497723a0fde401d6e
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-02-23 11:06:45 +0300

    FPS - devam
    Fps 7.hafta 1.ders

---
+++ FPS-1/Assets/scripts/human.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class human : MonoBehaviour
+{
+    public Transform freeMode, zoomMode;
+    public weapons weapon;
+    public Light flashLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FPS-1/Assets/scripts/weapons.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapons : MonoBehaviour
+{
+    public float range, demage, fireRate;
+    public AudioClip sound;
+    protected int currentBullets;
+    public Light muzzleLight;
+    public int maxBullets;
+    public float clipRate;
+    private float muzzleRate;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
