7d2ed0da5da55373f3294ce829c6619c48aa427a
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-03-16 11:55:53 +0300

    Fps - Enemy Script
    Fps devamı - 10.hafta

---
+++ FPS-1/Assets/scripts/enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject zombie;
+    public GameObject hero;
+    public float life;
+    public int speed;
+    public int limit;
+    public float damage;
+    public bool isCreated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createZombie();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public bool createZombie()
+    {
+        while (!isCreated)
+        {
+            GameObject z = Instantiate<GameObject>(zombie, new Vector3((hero.transform.position.x + Random.insideUnitSphere.x * 10),
+                hero.transform.position.y,
+                (hero.transform.position.z + Random.insideUnitSphere.z * 10)), Quaternion.identity);
+            limit--;
+            if (limit < 0)
+            {
+                isCreated = false;
+                break;
+            }
+        }
+        return false;
+    }
+}
