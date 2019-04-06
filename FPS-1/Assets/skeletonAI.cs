f594c2bc3233a709f639ad96880748ee5e28f5ce
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-04-06 13:23:46 +0300

    13.Hafta Ders sonu

---
+++ FPS-1/Assets/skeletonAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skeletonAI : MonoBehaviour
+{
+    private Vector3 startingPoint;
+    public float life;
+    public int scoreModifier = 100;
+    public bool dead = false;
+    private Animation anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        startingPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
