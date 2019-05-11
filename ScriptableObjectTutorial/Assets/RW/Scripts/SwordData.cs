99adc928b3ab04242ff1d246d1ce259cae3e90c4
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-05-11 13:22:55 +0300

    Scriptable Object Continue

---
+++ ScriptableObjectTutorial/Assets/RW/Scripts/SwordData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Create Sword Data", menuName = "Sword Data", order = 11)]
+public class SwordData : ScriptableObject
+{
+    [SerializeField]
+    private string swordName;
+    [SerializeField]
+    private string description;
+    [SerializeField]
+    private Sprite icon;
+    [SerializeField]
+    private int goldCost;
+    [SerializeField]
+    private float attackDamage;
+
+    public string SwordName
+    {
+        get
+        {
+            return swordName;
+        }
+    }
+    public string Description
+    {
+        get
+        {
+            return description;
+        }
+    }
+    public Sprite Icon
+    {
+        get
+        {
+            return icon;
+        }
+    }
+    public int GoldCost
+    {
+        get
+        {
+            return goldCost;
+        }
+    }
+    public float AttackDamage
+    {
+        get
+        {
+            return attackDamage;
+        }
+    }
+}
