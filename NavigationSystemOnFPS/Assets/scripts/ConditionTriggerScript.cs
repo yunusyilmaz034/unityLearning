994593fa0ff6de482d4bc2b5173622e5a8ed2412
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-06-22 13:34:38 +0300

    Sci_Fİ FPS
    Sci_Fİ FPS

--- NavigationSystemOnFPS/Assets/scripts/ConditionTriggerScript.cs
+++ NavigationSystemOnFPS/Assets/scripts/ConditionTriggerScript.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ConditionTriggerScript : MonoBehaviour
 {
     [SerializeField]
     private GameObject door;
+    [SerializeField]
+    private Text doorBell;
+
     private void OnTriggerEnter(Collider other)
     {
+        doorBell.gameObject.SetActive(true);
         door.GetComponent<doorTrigger>().openSusame();
     }
     private void OnTriggerExit(Collider other)
