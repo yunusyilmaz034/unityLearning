65fd5746c31e28b92d113ec7c3df45933a949418
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-06-22 13:23:24 +0300

    Sci_Fi_FPS_NavSystem
    Sci_Fi_FPS_NavSystem starter

---
+++ NavigationSystemOnFPS/Assets/scripts/TriggerTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject door;
+    private void OnTriggerEnter(Collider other)
+    {
+        door.GetComponent<doorTrigger>().openSusame();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        door.GetComponent<doorTrigger>().shutDownSusame();
+    }
+}
