4352942a6d008c4c82e5ddf0137db0cb0dc03e0a
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-06-29 14:06:26 +0300

    22.hafta son

--- NavigationSystemOnFPS/Assets/scripts/GlassDoorTrigger.cs
+++ NavigationSystemOnFPS/Assets/scripts/GlassDoorTrigger.cs
@@ -4,5 +4,47 @@ using UnityEngine;
 
 public class GlassDoorTrigger : MonoBehaviour
 {
-   
+    [SerializeField]
+    private GameObject door;
+    [SerializeField]
+    private GameObject infoPanel;
+    [SerializeField]
+    private bool autoClosed = true;
+
+    private bool isTriggerPanel = false;
+
+    private void Update()
+    {
+        if (isTriggerPanel)
+        {
+            if (Input.GetAxis("Door Bell") > 0) //E bastın mı
+            {
+                if (door.GetComponent<Animator>().GetBool("character_nearby") && !autoClosed)
+                {
+                    door.GetComponent<doorTrigger>().shutDownSusame();
+                }
+                else if (!door.GetComponent<Animator>().GetBool("character_nearby"))
+                {
+                    door.GetComponent<doorTrigger>().openSusame();
+                    
+                }
+                isTriggerPanel = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isTriggerPanel = true;
+        infoPanel.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isTriggerPanel = false;
+        infoPanel.SetActive(false);
+        if (autoClosed)
+        {
+            door.GetComponent<doorTrigger>().shutDownSusame();
+        }
+    }
 }
