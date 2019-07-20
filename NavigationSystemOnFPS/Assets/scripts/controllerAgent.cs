96deff7a95a15fce55f40b2a0872d458f27257b2
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-07-20 12:33:36 +0300

    25.hafta
    navigation system completed.

--- NavigationSystemOnFPS/Assets/scripts/controllerAgent.cs
+++ NavigationSystemOnFPS/Assets/scripts/controllerAgent.cs
@@ -5,21 +5,28 @@ using UnityEngine.AI;
 
 public class controllerAgent : MonoBehaviour
 {
-    public GameObject destinationTransform;
+    public Camera mainCam;
     private NavMeshAgent agent;
-
-    public bool isWalk = false;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
-        agent.destination = destinationTransform.transform.position;
+     //   agent.destination = destinationTransform.transform.position;
     }
-
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Direction") > 0) // mouse left click
+        {
+            RaycastHit rh;
+      
+            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out rh, 100))
+            {
+                agent.destination = rh.point;
+            }
+           
+        }
         
     }
 }
