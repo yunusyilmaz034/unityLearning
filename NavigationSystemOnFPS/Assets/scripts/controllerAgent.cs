e9dc91ff341525caf160d6a0a6a934162d70f7db
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-07-20 11:36:30 +0300

    25.hafta
    Navigation System Tutorial 1

---
+++ NavigationSystemOnFPS/Assets/scripts/controllerAgent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class controllerAgent : MonoBehaviour
+{
+    public GameObject destinationTransform;
+    private NavMeshAgent agent;
+
+    public bool isWalk = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.destination = destinationTransform.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
