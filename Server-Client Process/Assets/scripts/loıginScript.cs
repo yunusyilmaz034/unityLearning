ec1e8b150288c558f24a901f3af8098fe298946c
yunusyilmaz034 <31314444+yunusyilmaz034@users.noreply.github.com>
2019-08-24 13:38:56 +0300

    28.hafta
    Server-Client Starter

---
+++ Server-Client Process/Assets/scripts/loıginScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class loıginScript : MonoBehaviour
+{
+    public Text message;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void onClick()
+    {
+        if (string.IsNullOrEmpty(PlayFabSettings.TitleId))
+        {
+            PlayFabSettings.TitleId = "144";
+        }
+        var req = new LoginWithCustomIDRequest { CustomId = "Sample Id1", CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(req, onSuccess, onFailure);
+    }
+    private void onSuccess (LoginResult result)
+    {
+        message.text = "Giriş İzni Verildi. Go Go Go ..."; ;
+    }
+    private void onFailure(PlayFabError error)
+    {
+        message.text = "Hata: " + error.GenerateErrorReport();
+    }
+}
+  
